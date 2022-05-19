8aab9b4348e5d61ac4f4a114d4480b8fcb21b1b9
Thavhat <tsiwant@telkom.co.za>
2022-05-19 13:05:15 +0200

    redo

--- main.h
+++ main.h
@@ -1,50 +1,34 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <sys/wait.h>
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <fcntl.h>
 #include <string.h>
-#include <signal.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <limits.h>
+#include <string.h>
 
 extern char **environ;
 
-/* PATH Shell Functions */
-
-/* Program Flow */
-
-int prompt(void);
-char *_read(void);
-char *_fullpathbuffer(char **av, char *PATH, char *copy);
-int checkbuiltins(char **av, char *buffer, int exitstatus);
-int _forkprocess(char **av, char *buffer, char *fullpathbuffer);
-
-/* String Helper Functions */
-
-char *_strdup(char *str);
-int _splitstring(char *str);
-int _strcmp(const char *s1, const char *s2);
-char *_strcat(char *dest, char *src);
-int _strlen(char *s);
-
-/*Tokenize & PATH Helper Functions*/
-
-char **tokenize(char *buffer);
-int _splitPATH(char *str);
-int _PATHstrcmp(const char *s1, const char *s2);
-char *_concat(char *tmp, char **av, char *tok);
-
-/*Other Helper Funcs*/
-
-char *_getenv(const char *name);
-int _env(void);
-void _puts(char *str);
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-
-#endif /* MAIN_H */
+char *show_input(void);
+void prompt(void);
+char *_strcat(char *src);
+int _strlen(char *str);
+void place(char *str);
+char *findfile(char *command);
+char *find_command(char *command);
+int compare(char *s1, char *s2);
+int _strcmpdir(char *s1, char *s2);
+int charput(char c);
+void place(char *str);
+char *str_concat(char *s1, char *s2);
+int lookforslash(char *cmd);
+int compareExit(char *s1, char *s2);
+int compareEnv(char *s1, char *s2);
+void execute_proc(char **cmd);
+char **identify_string(char *parameter);
+void controlC(int sig);
+#endif
--- shell.c
+++ shell.c
@@ -1,40 +1,17 @@
-#include "main.h"
-/**
- * main - runs the shell program
- *
- * Return: 0 on success
- */
-int main(void)
-{
-	char *fullpathbuffer = NULL, *copy = NULL, *buffer = NULL;
-	char *PATH = NULL;
-	char **av = NULL;
-	int exitstatus = 0;
+#include "holberton.h"
 
-	signal(SIGINT, SIG_IGN);
-	PATH = _getenv("PATH");
-	if (PATH == NULL)
-		return (-1);
-	while (1)
-	{
-		av = NULL;
-		prompt();
-		buffer = _read();
-		if (*buffer != '\0')
-		{
-			av = tokenize(buffer);
-			if (av == NULL)
-			{
-				free(buffer);
-				continue;
-			}
-			fullpathbuffer = _fullpathbuffer(av, PATH, copy);
-			if (checkbuiltins(av, buffer, exitstatus) != 0)
-				continue;
-			exitstatus = _forkprocess(av, buffer, fullpathbuffer);
-		}
-		else
-			free(buffer);
-	}
+/**
+ * main - func with infinite loop
+ * @ac: No use
+ * @av: No use
+ * Return: loop.
+ **/
+int main(int ac, char **av)
+{
+	(void)av;
+	(void)ac;
+
+	signal(SIGINT, controlC);
+	prompt();
 	return (0);
 }
